e5fcaf063964a3254650a10c81d0f2ce90e18746
Mr-Fantastic1 <mail.for.registr500@gmail.com>
2018-03-03 15:53:19 +0500

    Add files via upload

--- academyheroes/js/main.js
+++ academyheroes/js/main.js
@@ -6,8 +6,36 @@ $(document).ready(function () {
     });
 
     $('.conditions__carusel').slick({
-        infinite: false,
         centerMode: true,
-        slidesToShow: 1
-    })
+        centerPadding: 'auto',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+
+    $('.next-shift__slider').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+});
+
+$('.faq-block__btn--answer').on('click', function() {
+    $('.faq-block__inner').removeClass('active');
+    $('.faq-block__answer').removeClass('active');
+    $('.faq-block__btn--answer').removeClass('hidden');
+    $('.faq-block__btn--roll-up').addClass('hidden');
+
+    $(this).closest('.faq-block__inner').addClass('active');
+    $(this).siblings('.faq-block__answer').addClass('active');
+    $(this).closest('.faq-block__btn--answer').addClass('hidden');
+    $(this).siblings('.faq-block__btn--roll-up').removeClass('hidden');
+});
+
+$('.faq-block__btn--roll-up').on('click', function() {
+    $('.faq-block__answer').removeClass('active');
+    $('.faq-block__btn--roll-up').addClass('hidden');
+    $('.faq-block__btn--answer').removeClass('hidden');
+    $(this).closest('.faq-block__inner').removeClass('active');
 });
