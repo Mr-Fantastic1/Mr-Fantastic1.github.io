a4d08c9755bcedb44bb1961410c9b23383a67209
Mr-Fantastic1 <mail.for.registr500@gmail.com>
2018-03-05 13:25:59 +0500

    Add files via upload

--- academyheroes/js/main.js
+++ academyheroes/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $('.command__wrapper').slick({
         infinite: false,
         slidesToShow: 6,
-        slidesToScroll: 6
+        slidesToScroll: 6,
     });
 
     $('.conditions__carusel').slick({
